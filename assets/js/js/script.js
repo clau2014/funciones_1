4b38e5f5b7b9f098a4579bd488169d44abd3553e
clau2014 <clau88and@gmail.com>
2024-04-07 03:34:00 -0400

    tercer commit

--- assets/js/js/script.js
+++ assets/js/js/script.js
@@ -1,24 +1,23 @@
-
+// Función para cambiar el color de fondo de un elemento
 function cambiarColor(elemento, color = 'green') {
     elemento.style.backgroundColor = color;
 }
 
-
+// Obtener el elemento con el ID "elem1"
 const elem = document.getElementById("elem1");
 
-
+// Obtener el elemento con el ID "clickMe" para cambiar a amarillo al hacer clic
 const clickMe = document.getElementById("clickMe");
 clickMe.addEventListener("click", function() {
-    
-    cambiarColor(elem, 'yellow');
+    cambiarColor(elem, 'yellow'); // Cambiar a amarillo al hacer clic en "clickMe"
 });
 
-
+// Agregar evento para cambiar el color a negro al hacer clic en "elem1"
 elem.addEventListener('click', function(event) {
-    cambiarColor(event.target, 'black');
+    cambiarColor(event.target, 'black'); // Cambiar a negro al hacer clic en "elem1"
 });
 
-
+// Agregar evento para crear un nuevo div con un color específico al presionar una tecla
 document.addEventListener('keydown', function(event) {
     let color;
     switch (event.key) {
@@ -35,6 +34,7 @@ document.addEventListener('keydown', function(event) {
             return;
     }
     
+    // Crear un nuevo div con el color especificado
     const nuevoDiv = document.createElement('div');
     nuevoDiv.style.width = '200px';
     nuevoDiv.style.height = '200px';
